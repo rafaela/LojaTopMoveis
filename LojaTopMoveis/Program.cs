0b433e6198c768f86e4fb83041face504220b851
Rafaela Paiva <rafaela.paiva@trybu.com.br>
2024-05-26 20:24:10 -0300

    voltando ao inicio Program.cs

--- LojaTopMoveis/Program.cs
+++ LojaTopMoveis/Program.cs
@@ -125,16 +125,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseRouting();
 
 app.UseCors(x => x.AllowAnyMethod()
                   .AllowAnyHeader()
